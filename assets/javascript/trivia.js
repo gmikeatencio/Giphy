45e179bdeae7be1263ae93ae298307e6715716a4
Hunter Leachman <Leachmanh@gmail.com>
2016-09-03 12:12:23 -0500

    changed question bank structure

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,26 +1,33 @@
 var questions = [{
-      "questionText" : "Hey, here is the question",        
-      "answer1": {
-          "text": "correct",
+      "questionText" : "Hey, here is the question", 
+      "answers": [{      
+      "answer": {
+          "text": "correct answer",
           "correct": true,
               },
-      "answer2": {
+      "answer": {
           "text": "new one",
           "correct": false,
               }
+      }]       
+      
   }, {
-      "questionText" : "Hey, here is another question",        
-      "answer1": {
-          "text": "correct 2",
+     "questionText" : "Hey, here is another question",
+
+     "answers": [{
+     "answer": {
+          "text": "correct answer 2",
           "correct": true,
               },
-      "answer2": {
+      "answer": {
           "text": "new one 2",
           "correct": false,
               }
+      }]           
   }]
 var correctCount = 0;
 var wrongCount = 0;
+var q=0;
 
 function questTimer() {
 var seconds_left = 10;
@@ -33,18 +40,25 @@ var interval = setInterval(function() {
     }
 }, 1000);
 }
-
-
 $('#startButt').on('click', function(){
 	$('.start').empty();
-	q=0;
-	$('.quest').html("<p>" + questions[q].questionText + "</p>");
-	questTimer();
-	$('.answ').append("<div class='ans'> <p>" + questions[q].answer1.text + "</p></div>");
-	$('.answ').append("<div class='ans'> <p>" + questions[q].answer2.text + "</p></div>");
-});
+  $('.quest').append("<p>" + questions[q].questionText + "</p>");
 
-//$('.answ').on('click', function(){
+for (var i in questions[q]) {
+   console.log(questions[q]);
+}
 
-	//if (questions[q])
-
+/*
+  for (i=0; i < questions[q].answers.length; i++){
+  $('.quest').append("<div class='answer'><p>" + questions[q].answers[i].answer.text + "</p></div>");
+  console.log(questions[q].answers.length);
+  console.log(questions[q].answers);
+  };
+  */
+  questTimer();
+  q++;
+  $('.answer').on('click', function(){
+    console.log("hey there");
+    console.log(this);
+  });
+  });
